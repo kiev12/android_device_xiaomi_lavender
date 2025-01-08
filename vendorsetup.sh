f23802a47740c6e4e5d2f0577def1218f9abf23c
kiev12 <124450781+kiev12@users.noreply.github.com>
2025-01-08 16:39:39 -0500

    Update vendorsetup.sh
    ................

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_lavender-user
-add_lunch_combo omni_lavender-userdebug
-add_lunch_combo omni_lavender-eng
+add_lunch_combo twrp_lavender-user
+add_lunch_combo twrp_lavender-userdebug
+add_lunch_combo twrp_lavender-eng
